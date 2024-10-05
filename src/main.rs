c0391fb20ba9ab47dd600d9e08d0531b3c80ddb7
Sebastian Schmidt <isibboi@gmail.com>
2024-10-05 14:44:15 +0300

    Benchmark tirangle wave dimming.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
-use std::{thread::sleep, time::Duration};
+use std::{
+    thread::sleep,
+    time::{Duration, Instant},
+};
 
 use rppal::pwm::{Channel, Polarity, Pwm};
 
+fn dim_triangle() {
+    let pwm = Pwm::with_frequency(Channel::Pwm0, 1e3, 0.0, Polarity::Normal, true).unwrap();
+
+    let mut intensity = 0.0;
+    let mut increase = true;
+    let start_time = Instant::now();
+
+    for _ in 0..2000 {
+        sleep(Duration::from_millis(1));
+
+        if increase {
+            intensity += 1e-3;
+            if intensity > 1.0 {
+                intensity = 1.0;
+                increase = false;
+            }
+        } else {
+            intensity -= 1e-3;
+            if intensity < 0.0 {
+                intensity = 0.0;
+                increase = true;
+            }
+        }
+
+        pwm.set_duty_cycle(intensity).unwrap();
+    }
+
+    let end_time = Instant::now();
+    let duration = end_time - start_time;
+    let duration = duration.as_secs_f64();
+    println!("Duration factor: {}", duration / 2.0);
+}
+
 fn main() {
-    println!("Starting PWM");
-
-    let mut pwm = Pwm::with_frequency(Channel::Pwm0, 1.0, 0.5, Polarity::Normal, true).unwrap();
-    pwm.set_reset_on_drop(false);
-
-    println!("PWM running, waiting forever");
-
-    sleep(Duration::from_secs(u64::MAX));
+    dim_triangle();
 }
