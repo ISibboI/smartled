5da240b6b2992b3ca3b84aab81b384769f44e7e6
Sebastian Schmidt <isibboi@gmail.com>
2024-10-05 14:33:09 +0300

    Transform qualified path into import.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::{thread::sleep, time::Duration};
 
-use rppal::pwm::{Polarity, Pwm};
+use rppal::pwm::{Channel, Polarity, Pwm};
 
 fn main() {
     println!("Starting PWM");
 
-    let _ =
-        Pwm::with_frequency(rppal::pwm::Channel::Pwm0, 1.0, 0.5, Polarity::Normal, true).unwrap();
+    let _ = Pwm::with_frequency(Channel::Pwm0, 1.0, 0.5, Polarity::Normal, true).unwrap();
 
     println!("PWM running, waiting forever");
 
