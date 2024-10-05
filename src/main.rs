0fcfcf1719b866c84038de23925d4ea56f44ac6a
Sebastian Schmidt <isibboi@gmail.com>
2024-10-05 14:02:51 +0300

    Set PWM to 0.1Hz with 0.5 duty cycle.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{thread::sleep, time::Duration};
+
+use rppal::pwm::{Polarity, Pwm};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting PWM");
+
+    let _ =
+        Pwm::with_frequency(rppal::pwm::Channel::Pwm0, 10.0, 0.5, Polarity::Normal, true).unwrap();
+
+    println!("PWM running, waiting forever");
+
+    sleep(Duration::from_secs(u64::MAX));
 }
